a1ade622ce775c74ca99d8df9c693da05ed3d65e
ojwatts1999 <60313545+ojwatts1999@users.noreply.github.com>
2021-04-16 12:58:26 +0100

    Cleanup files if the spam gets too crazy.

---
+++ clean.cmd
@@ -0,0 +1,17 @@
+@echo off
+taskkill /f /im python.exe
+taskkill /f /im flashintothepast.exe
+taskkill /f /im flash1.exe
+taskkill /f /im flash2.exe
+taskkill /f /im flash3.exe
+taskkill /f /im flash4.exe
+taskkill /f /im flash5.exe
+taskkill /f /im flash6.exe
+taskkill /f /im flash7.exe
+taskkill /f /im flash8.exe
+taskkill /f /im flash9.exe
+taskkill /f /im flash10.exe
+taskkill /f /im flash11.exe
+taskkill /f /im flash12.exe
+taskkill /f /im tour.exe
+exit
